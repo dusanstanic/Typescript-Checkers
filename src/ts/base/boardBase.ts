6f8786f52cca3742cdec14ddec33157e2b2cd230
Dusan Stanic <dusan.stanic97@hotmail.com>
2021-03-17 12:01:21 +0100

    Update boarderBase and ChoiceBase

--- src/ts/base/boardBase.ts
+++ src/ts/base/boardBase.ts
@@ -1,5 +1,8 @@
+import choiceElements from "./choiceBase";
+
 const elements = {
   board: document.querySelector(".board"),
+  newGameBtn: choiceElements.newGameBtn,
 };
 
 export default elements;
---
+++ src/ts/base/choiceBase.ts
@@ -0,0 +1,8 @@
+const choices = document.querySelectorAll(".choice__item");
+
+const elements = {
+  newGameBtn: choices[0],
+  finishTurnBtn: choices[1],
+};
+
+export default elements;
