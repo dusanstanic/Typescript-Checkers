94d7dfb5f0cc9dfe03a53ca6cb71fce7a7bb5990
Dusan Stanic <dusan.stanic97@hotmail.com>
2021-03-17 11:59:49 +0100

    Update checker state

--- src/ts/checkerState.ts
+++ src/ts/checkerState.ts
@@ -6,16 +6,17 @@ interface CheckerState {
   blackCheckersCount: number;
 }
 
-const checkerState: CheckerState = {
+const checkerInitialState: CheckerState = {
   moveHistory: [],
   movesMade: 0,
   activePlayer: "red",
-  redCheckersCount: 0,
-  blackCheckersCount: 0,
+  redCheckersCount: 12,
+  blackCheckersCount: 12,
 };
 
+let checkerState: CheckerState = { ...checkerInitialState };
+
 export const checkIfPlayerWon = () => {
-  console.log(checkerState);
   if (checkerState.redCheckersCount === 0) {
     alert("Player black won!");
   }
@@ -23,6 +24,39 @@ export const checkIfPlayerWon = () => {
   if (checkerState.blackCheckersCount === 0) {
     alert("Player red won!");
   }
+};
+
+export const finishTurn = () => {
+  checkerState.activePlayer === "red"
+    ? (checkerState.activePlayer = "black")
+    : (checkerState.activePlayer = "red");
+
+  checkerState.movesMade = 0;
+  checkerState.moveHistory = [];
+
+  updateCheckerUI();
+};
+
+export const resetCheckerState = () => {
+  checkerState = { ...checkerInitialState };
+};
+
+export const updateCheckerUI = () => {
+  const activeBtn = document.querySelector(".active");
+  activeBtn?.classList.remove("active");
+
+  const playerTurn = <HTMLDivElement>document.querySelector(".player__turn");
+  playerTurn.textContent = `Current Player: ${checkerState.activePlayer.toUpperCase()}`;
+
+  if (checkerState.activePlayer === "black") {
+    playerTurn.classList.add("player__turn--black");
+  } else {
+    playerTurn.classList.remove("player__turn--black");
+  }
+
+  const checkerAmount = document.querySelectorAll(".player__amount");
+  checkerAmount[0].textContent = "Total: " + checkerState.redCheckersCount;
+  checkerAmount[1].textContent = "Total: " + checkerState.blackCheckersCount;
 };
 
 export default checkerState;
