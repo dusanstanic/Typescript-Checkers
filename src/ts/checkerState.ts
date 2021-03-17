bc5b34e74d73f55a4508adf64a659ba13ffadcaf
Dusan Stanic <dusan.stanic97@hotmail.com>
2021-03-17 08:25:32 +0100

    Update CheckerState

--- src/ts/checkerState.ts
+++ src/ts/checkerState.ts
@@ -5,4 +5,15 @@ const checkerState = {
   blackCheckersCount: 0,
 };
 
+export const checkIfPlayerWon = () => {
+  console.log(checkerState);
+  if (checkerState.redCheckersCount === 0) {
+    alert("Player black won!");
+  }
+
+  if (checkerState.blackCheckersCount === 0) {
+    alert("Player red won!");
+  }
+};
+
 export default checkerState;
